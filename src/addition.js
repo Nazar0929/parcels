8c2b71922280a6e994c6e8891ef3b30e8397318c
Nazar0929 <166243366+Nazar0929@users.noreply.github.com>
2025-10-10 20:06:32 +0300

    push

---
+++ src/addition.js
@@ -0,0 +1,44 @@
+// // export default може експортувати щось одне
+
+// export default function add(a, b) {
+//     return a + b;
+
+// }
+
+
+// іменований експорт
+
+// export function add(a,b) {
+//     return a + b;
+// }
+
+
+// export function subtraction(a,b) {
+//     return a - b;
+// }
+
+
+
+
+
+// function add(a,b) {
+//     return a + b;
+// }
+
+
+//  function subtraction(a,b) {
+//     return a - b;
+// }
+
+
+// export default {add, subtraction}
+
+
+export function add(a,b) {
+    return a + b;
+}
+
+
+export function subtraction(a,b) {
+    return a - b;
+}
--- src/app.js
+++ src/app.js
@@ -1,1 +1,57 @@
-console.log("Hello Leon");
+
+
+// // дефолтний імпорт
+
+// import added from "./addition"
+
+// const result = added(2,9)
+// console.log(result);
+
+
+// іменований імпорт
+
+// import { add, subtraction } from "./addition";
+// const result = add(2,92);
+// console.log(result);
+
+// const results = subtraction(2,92);
+// console.log(results);
+
+
+
+// import calculate from "./addition"
+// console.log(calculate);
+
+// const result3 = calculate.add(11,4);
+// const result4 = calculate.subtraction(11,4);
+// console.log(result3);
+// console.log(result4);
+
+
+
+import { nanoid } from "nanoid";
+
+const people = [
+  { name: "Іса", lastName: "Петренко", age: 25 },
+  { name: "Олена", lastName: "Ковальчук", age: 32 },
+  { name: "Максим", lastName: "Сидоренко", age: 28 },
+  { name: "Аліна", lastName: "Ткаченко", age: 22 },
+  { name: "Юрій", lastName: "Мельник", age: 35 },
+  { name: "Наталя", lastName: "Іванова", age: 30 },
+  { name: "Дмитро", lastName: "Бондар", age: 27 },
+  { name: "Світлана", lastName: "Кравченко", age: 29 },
+  { name: "Артем", lastName: "Гончар", age: 31 },
+  { name: "Марина", lastName: "Литвин", age: 26 }
+];
+
+const formatId = people.map(user => ({...user, id : nanoid()}))
+console.log(formatId);
+
+
+import * as calculate from "./addition"
+console.log(calculate);
+
+const result3 = calculate.add(11,4);
+const result4 = calculate.subtraction(11,4);
+console.log(result3);
+console.log(result4);
